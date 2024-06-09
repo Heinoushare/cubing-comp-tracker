0778cd67589ca05bb30ec73527b43027f83629ae
Heinoushare <samuel.sudbury@gmail.com>
2024-06-09 18:26:20 -0400

    Basic comp in zone detection working

--- src/routes/competitions/zones/[zone_id]/+page.svelte
+++ src/routes/competitions/zones/[zone_id]/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     /** @type {import('./$types').PageData} */
 	export let data;
-
     
+
+    let competitions = data.competitionsInZone;
+
     let zone = data.zone;
 
     let latitude = zone["latitude"];
@@ -10,25 +12,54 @@
     let radius = zone["radius"];
     let unit = zone["radius_units"];
 
+    let editZone = false;
+    function edit() {
+        editZone = true;
+    }
+
 </script>
 
-<h5>Edit your zone.</h5>
-<form method="POST">
-    <label>Location</label>
-    <input bind:value={latitude} type="number" placeholder="Latitude" name="latitude" step="0.000000000000000001" autocomplete="off" required>
-    <input bind:value={longitude} type="number" placeholder="Longitude" name="longitude" step="0.000000000000000001" autocomplete="off" required>
-    <br>
-    <label>Range</label>
-    <input bind:value={radius} type="number" placeholder="Radius" name="radius" autocomplete="off" required>
-    <select name="unit" required>
-        {#if unit === "miles"}
-            <option value="miles" selected>Miles</option>
-            <option value="kilometers">Kilometers</option>
-        {:else if unit === "kilometers"}
-            <option value="miles">Miles</option>
-            <option value="kilometers" selected>Kilometers</option>
-        {/if}
-    </select>
-    <br>
-    <button type="submit" class="btn btn-primary">Edit</button>
-</form>
+
+<h5>Upcoming Competitions in your zone:</h5>
+<br>
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Date</th>
+    </tr>
+
+    {#each competitions as comp, i}
+
+        <tr>
+            <td>{comp["name"]}</td>
+            <td>From {comp["date"]["from"]} till {comp["date"]["till"]}</td>
+        </tr>
+
+    {/each}
+
+</table>
+
+<button on:click={edit} class="btn btn-primary">Edit Zone</button>
+
+{#if editZone}
+    <h5>Edit your zone.</h5>
+    <form method="POST">
+        <label>Location</label>
+        <input bind:value={latitude} type="number" placeholder="Latitude" name="latitude" step="0.000000000000000001" autocomplete="off" required>
+        <input bind:value={longitude} type="number" placeholder="Longitude" name="longitude" step="0.000000000000000001" autocomplete="off" required>
+        <br>
+        <label>Range</label>
+        <input bind:value={radius} type="number" placeholder="Radius" name="radius" autocomplete="off" required>
+        <select name="unit" required>
+            {#if unit === "miles"}
+                <option value="miles" selected>Miles</option>
+                <option value="kilometers">Kilometers</option>
+            {:else if unit === "kilometers"}
+                <option value="miles">Miles</option>
+                <option value="kilometers" selected>Kilometers</option>
+            {/if}
+        </select>
+        <br>
+        <button type="submit" class="btn btn-primary">Edit</button>
+    </form>
+{/if}
